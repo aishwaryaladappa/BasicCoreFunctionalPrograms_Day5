f8348e2bd4599498f220c496f9016437738085d4
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-10-09 17:51:31 +0530

    greeting message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace basic_core_program
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core programs");
+        }
+
+    }
+
+}
